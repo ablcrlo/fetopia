5009c6629faaa66271e6e800986acba56fd3576e
fetopia <abelcarlojoaquin@gmail.com>
2023-05-10 10:15:37 +0800

    commit message

---
+++ fb.py
@@ -0,0 +1,59 @@
+import nltk
+nltk.download('punkt')
+import pickle
+import tensorflow as tf
+import numpy as np
+import random
+import json
+from nltk.stem import WordNetLemmatizer
+lemmatizer = WordNetLemmatizer()
+
+with open('intents.json') as file:
+    data = json.load(file)
+
+with open('words.pkl', 'rb') as f:
+    words = pickle.load(f)
+with open('classes.pkl', 'rb') as f:
+    classes = pickle.load(f)
+model = tf.keras.models.load_model('chatbot_model.h5')
+
+def clean_up_sentence(sentence):
+    sentence_words = nltk.word_tokenize(sentence)
+    sentence_words = [lemmatizer.lemmatize(word.lower()) for word in sentence_words]
+    return sentence_words
+
+def bag_of_words(sentence):
+    sentence_words = clean_up_sentence(sentence)
+    bag = [0] * len(words)
+    for w in sentence_words:
+        for i, word in enumerate(words):
+            if word == w:
+                bag[i] = 1
+    return np.array(bag)
+
+def predict_class(sentence):
+    bow = bag_of_words(sentence)
+    res = model.predict(np.array([bow]))[0]
+    ERROR_THRESHOLD = 0.25
+    results = [[i, r] for i, r in enumerate(res) if r > ERROR_THRESHOLD]
+    results.sort(key=lambda x: x[1], reverse=True)
+    return_list = []
+    for r in results:
+        return_list.append({'intent': classes[r[0]], 'probability': str(r[1])})
+    return return_list
+
+def get_response(intents_list, intents_json):
+    tag = intents_list[0]['intent']
+    list_of_intents = intents_json['intents']
+    for i in list_of_intents:
+        if i['tag'] == tag:
+            result = random.choice(i['responses'])
+            break
+    return result
+
+print("Go! Bot is running!")
+while True:
+    message = input("")
+    ints = predict_class(message)
+    res = get_response(ints, data)
+    print(res)
